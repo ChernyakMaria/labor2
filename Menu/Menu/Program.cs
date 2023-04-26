68548666d2bec45d204a59d44a8bbe3f7ec8239b
Halobrobro <Halobrobro@mail.ru>
2023-04-26 20:13:16 +0700

    case /

--- Menu/Menu/Program.cs
+++ Menu/Menu/Program.cs
@@ -5,25 +5,25 @@ namespace Menu
     {
 		static void Main(string[] args)
 		{
-			double a, b;
-			Console.WriteLine("Введите А: ");
-			a = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("Введите В: ");
-			b = Convert.ToDouble(Console.ReadLine());
 			char znak;
 			Console.WriteLine("Введите знак для реализации операции:");
-			Console.WriteLine("(-)");
-			Console.WriteLine("(+)");
-			Console.WriteLine("(*)");
 			Console.WriteLine("(/)");
 			znak = Convert.ToChar(Console.ReadLine());
 
 			switch (znak)
 			{
+				case '/':
+					if (a != 0 || b != 0)
+					{
 
-				default:
-					Console.WriteLine("Ошибка! Неизвестная математическая операция. ");
+						Console.WriteLine("Результат: ", (float)a / b);
+					}
+					else if (a == 0 || b == 0)
+					{
+						Console.WriteLine("На ноль делить нельзя.");
+					}
 					break;
+
 			}
-	}	}
+		}	}
 }
