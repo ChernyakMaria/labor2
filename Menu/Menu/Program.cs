384ee3b010380aafb1255d4872379212c67f0c96
Halobrobro <Halobrobro@mail.ru>
2023-04-26 20:04:45 +0700

    viborA

--- Menu/Menu/Program.cs
+++ Menu/Menu/Program.cs
@@ -8,22 +8,7 @@ namespace Menu
 			double a, b;
 			Console.WriteLine("Введите А: ");
 			a = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("Введите В: ");
-			b = Convert.ToDouble(Console.ReadLine());
-			char znak;
-			Console.WriteLine("Введите знак для реализации операции:");
-			Console.WriteLine("(-)");
-			Console.WriteLine("(+)");
-			Console.WriteLine("(*)");
-			Console.WriteLine("(/)");
-			znak = Convert.ToChar(Console.ReadLine());
 
-			switch (znak)
-			{
-
-				default:
-					Console.WriteLine("Ошибка! Неизвестная математическая операция. ");
-					break;
-			}
-	}	}
+		}
+	}
 }
