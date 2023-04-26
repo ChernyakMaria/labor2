a93dbc1b719f8b3077bf8b7d003a32f5f3a946dd
Halobrobro <Halobrobro@mail.ru>
2023-04-26 18:56:08 +0700

    new

---
+++ Menu/Menu/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			double a, b;
+			Console.WriteLine("Введите А: ");
+			 a = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Введите В: ");
+			b = Convert.ToDouble(Console.ReadLine());
+			char znak;
+			Console.WriteLine("Введите знак для реализации операции:");
+			Console.WriteLine("(-)");
+			Console.WriteLine("(+)");
+			Console.WriteLine("(*)");
+			Console.WriteLine("(/)");
+			znak = Convert.ToChar(Console.ReadLine());
+		}
+    }
+}
