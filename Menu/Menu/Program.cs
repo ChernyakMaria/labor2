761cbc0dcd8b2964961f80dabf05450c977c5004
Halobrobro <Halobrobro@mail.ru>
2023-04-26 20:12:03 +0700

    case *

--- Menu/Menu/Program.cs
+++ Menu/Menu/Program.cs
@@ -5,15 +5,9 @@ namespace Menu
     {
 		static void Main(string[] args)
 		{
-			double a, b;
-			Console.WriteLine("Введите А: ");
-			a = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("Введите В: ");
-			b = Convert.ToDouble(Console.ReadLine());
+
 			char znak;
 			Console.WriteLine("Введите знак для реализации операции:");
-			Console.WriteLine("(-)");
-			Console.WriteLine("(+)");
 			Console.WriteLine("(*)");
 			Console.WriteLine("(/)");
 			znak = Convert.ToChar(Console.ReadLine());
@@ -21,9 +15,10 @@ namespace Menu
 			switch (znak)
 			{
 
-				default:
-					Console.WriteLine("Ошибка! Неизвестная математическая операция. ");
+				case '*':
+					Console.WriteLine("Результат: ", a * b);
 					break;
+
 			}
-	}	}
+		}	}
 }
