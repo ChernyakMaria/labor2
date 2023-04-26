1bf7fb9cfded3382802af7729f6ba7f5b7084761
Halobrobro <Halobrobro@mail.ru>
2023-04-26 21:15:19 +0700

    fix

--- Menu/Menu/Program.cs
+++ Menu/Menu/Program.cs
@@ -5,25 +5,10 @@ namespace Menu
 	{
 		static void Main(string[] args)
 		{
-			double a;
 			double b;
-			double total;
-			Console.WriteLine("Введите А: ");
-			a = Convert.ToDouble(Console.ReadLine());
 			Console.WriteLine("Введите B: ");
 			b = Convert.ToDouble(Console.ReadLine());
-			char znak;
-			Console.WriteLine("Введите знак для реализации операции:");
-			Console.WriteLine("(+)");
-			Console.WriteLine("(-)");
-			Console.WriteLine("(*)");
-			Console.WriteLine("(/)");
-			znak = Convert.ToChar(Console.ReadLine());
-			switch (znak)
-			{
-
-
-			}
+			
 
 		}
 
